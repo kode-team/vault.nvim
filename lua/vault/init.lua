2e2ecb006faf6e3c193f67ef2c41067808df7bf4
Jaeyeol Lee <rijgndqw012@gmail.com>
2023-04-12 01:33:55 +0900

    Add `open_vault()` command in order to open encrypted vault

--- lua/vault/init.lua
+++ lua/vault/init.lua
@@ -81,6 +81,60 @@ M.setup = function(opts)
       content = content,
     })
   end
+
+  vim.g.nvim_vault_cipher_algorithm = cipher_algorithm
+  vim.g.nvim_vault_key_path = key_path
+  vim.g.nvim_encrypted_vault_path = encrypted_file_path
+end
+
+M.find_buffer_by_name = function(name)
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    local buf_name = vim.api.nvim_buf_get_name(buf)
+    if string.find(buf_name, name) then
+      return buf
+    end
+  end
+  return -1
+end
+
+M.open_vault = function()
+  local cipher_algorithm = vim.g.nvim_vault_cipher_algorithm
+  local key_path = vim.g.nvim_vault_key_path
+  local encrypted_file_path = vim.g.nvim_encrypted_vault_path
+
+  local key_file = io.open(path:new(key_path):expand(), "r")
+  local key = key_file:read()
+  key_file:close()
+
+  local encrypted_file = io.open(path:new(encrypted_file_path):expand(), "r")
+  local encrypted_content = encrypted_file:read()
+  encrypted_file:close()
+
+  local content = cipher.decrypt(cipher_algorithm, encrypted_content, key)
+
+  local buf = nil
+  local bufnr = 0
+  local target_buf_name = "__VAULT__"
+  local target_buffer = M.find_buffer_by_name(target_buf_name)
+  if target_buffer ~= -1 then
+    buf = target_buffer
+    vim.api.nvim_buf_delete(buf, {})
+
+    buf = vim.api.nvim_create_buf(true, true)
+  else
+    vim.cmd("vsplit")
+    buf = vim.api.nvim_create_buf(true, true)
+  end
+
+  local win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(win, buf)
+
+  bufnr = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_set_name(bufnr, target_buf_name)
+  vim.api.nvim_buf_set_option(bufnr, "filetype", "jsonvault")
+
+  vim.api.nvim_buf_set_lines(bufnr, 0, 0, "true", { content })
 end
 
 return M
